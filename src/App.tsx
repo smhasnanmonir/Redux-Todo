8376af49c565098f72a7beba795b8495f9a6aaed
smhasnanmonir <monirhasnan@gmail.com>
2024-08-29 19:13:17 +0600

    counter added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { decrement, increment } from "./redux/features/counterSlice";
 
 function App() {
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   return (
-    <>
-      <h1 className="font-extrabold">Hello</h1>
-    </>
+    <div className="grid place-content-center py-[10%]">
+      <div className="px-[10%] py-[5%] bg-slate-200 border border-black w-[400px] flex">
+        <button
+          onClick={() => dispatch(increment(count))}
+          className="rounded-md bg-green-400 font-medium text-xl px-3 py-2"
+        >
+          Increment
+        </button>
+        <h1 className="text-3xl mx-10">{count}</h1>
+        <button
+          onClick={() => dispatch(decrement(count))}
+          className="rounded-md bg-red-400 font-medium text-xl px-3 py-2"
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
   );
 }
 
